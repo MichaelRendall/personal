0c7ad0cd81c4dec1682eb12b81d643dce47918a3
Michael Rendall <michaelrendallmail@gmail.com>
2022-03-02 20:15:00 +0000

    got initial connection to websocket

--- src/components/paperGame/Game.tsx
+++ src/components/paperGame/Game.tsx
@@ -7,6 +7,8 @@ import Button from "../FormElements/Button";
 import Wrapper from "../UI/Wrapper";
 import SubmissionsForm from "./SubmissionsForm";
 
+import { io } from "socket.io-client";
+
 interface GameProps {
   leaveGame: (event: React.FormEvent) => Promise<void>;
 }
@@ -14,6 +16,21 @@ interface GameProps {
 const Game: React.FC<GameProps> = (props) => {
   const { data, sendRequest } = useFetch();
   const [cookies] = useCookies(["uuid", "name", "roomId"]);
+
+  useEffect(() => {
+    const socket = io("http://localhost:8080");
+    socket.on("paper-game", (data) => {
+      if (data.action === "joined") {
+        const fetchGame = async () => {
+          sendRequest({
+            url: `http://localhost:8080/get-game/${cookies.roomId}`,
+            method: "GET",
+          });
+        };
+        fetchGame();
+      }
+    });
+  }, [cookies.roomId, sendRequest]);
 
   useEffect(() => {
     const fetchGame = async () => {
@@ -24,7 +41,7 @@ const Game: React.FC<GameProps> = (props) => {
     };
     fetchGame();
   }, [cookies.roomId, sendRequest]);
-  console.log(data);
+
   return (
     <div className={classes.game}>
       {data && (
