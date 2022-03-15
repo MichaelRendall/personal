2d33e1309033731a8a3cca8b334f222fcdb8164e
Michael Rendall <michaelrendallmail@gmail.com>
2022-03-15 13:29:12 +0000

    made small start to flag quiz

---
+++ src/lib/flag-list.ts
@@ -0,0 +1,7 @@
+const FLAG_LIST = [
+  { name: "France", continent: "europe", src: "france.jpg" },
+  { name: "Germany", continent: "europe", src: "germany.png" },
+  { name: "Australia", continent: "oceania", src: "australia.jpg" },
+];
+
+export default FLAG_LIST;
--- src/pages/Flags.tsx
+++ src/pages/Flags.tsx
@@ -1,16 +1,46 @@
-import React, { useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
+import Button from "../components/FormElements/Button";
+import GameHeading from "../components/GameHeading/GameHeading";
 import GameSection from "../components/UI/GameSection";
 import { ThemeContext } from "../context/theme-context";
 import Theme from "../models/theme-enum";
+import FLAG_LIST from "../lib/flag-list";
+
+interface FlagList {
+  [key: string]: string;
+  name: string;
+  continent: string;
+  src: string;
+}
 
 const Flags = () => {
   document.title = "Flags | Michael Rendall";
   const themeCtx = useContext(ThemeContext);
-  
+
   useEffect(() => {
     themeCtx.changeTheme(Theme.BLUE);
   }, [themeCtx]);
-  return <GameSection>Flag Quiz Coming Soon</GameSection>;
+
+  const [currentFlag, setCurrentFlag] = useState<FlagList>();
+  //const [activeFlags, setActiveFlags] = useState<FlagList[]>(FLAG_LIST);
+
+  const startGameHandler = () => {
+    //const flag = activeFlags[Math.floor(Math.random() * activeFlags.length)];
+    const flag = FLAG_LIST[Math.floor(Math.random() * FLAG_LIST.length)];
+    setCurrentFlag(flag);
+  };
+
+  return (
+    <GameSection>
+      <GameHeading heading="FLAG QUIZ" showSettings />
+      <Button onClick={startGameHandler} name="Begin" large />
+      <p>{currentFlag?.name}</p>
+      <img
+        src={`../assets/flags/${currentFlag?.src}`}
+        alt={`flag of ${currentFlag?.name}`}
+      />
+    </GameSection>
+  );
 };
 
 export default Flags;
